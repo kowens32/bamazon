a7184c9017c631fe9e87ae0d83125b2721ad2eaf
kimberly owens <kimberlyowens@MacBook-Pro-2.local>
2017-10-28 16:36:48 -0400

    initial

---
+++ bamazon.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS bamazon ;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+
+CREATE TABLE products (
+item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+product_name
+department_name VARCHAR(30) NOT NULL
+price
+stock_quantity
+
+
+
+
+
+
+)
